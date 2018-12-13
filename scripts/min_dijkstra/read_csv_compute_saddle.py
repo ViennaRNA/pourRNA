b1052980ed2e4badeb595a23fbc45825c5699e8a
Gregor Entzian <g.entzian@viennarna.at>
2018-12-13 16:12:42 +0100

    +plot refolding path energies

--- scripts/min_dijkstra/read_csv_compute_saddle.py
+++ scripts/min_dijkstra/read_csv_compute_saddle.py
@@ -7,12 +7,14 @@ from operator import itemgetter
 #from scipy.sparse.csgraph._shortest_path import shortest_path
 import math
 import re
+import matplotlib.pyplot as plt
 
 """
 format: id_from, structure from, energy from, id_to, str. to, energy to, saddle str, saddle energy. 
 """
 def read_saddle_csv_file(file):
     paths_dictionary = {}
+    struct_id_energy_dict = {}
     min_saddle = 1000000000
     with open(file, 'r') as f:
         new_index = 0
@@ -29,12 +31,12 @@ def read_saddle_csv_file(file):
                 
                 index_from = match.group(1)
                 #structure_from = match.group(2)
-                #energy_from = match.group(3)
+                energy_from = match.group(3)
                 #energy_from = float(energy_from)
                 
                 index_to = match.group(4)
                 #structure_to = match.group(5)
-                #energy_to = match.group(6)
+                energy_to = match.group(6)
                 #energy_to = float(energy_to)
                 
                 #structure_saddle = match.group(7)
@@ -47,6 +49,11 @@ def read_saddle_csv_file(file):
                 except Exception as e:
                     print("Error: could not parse index from input file!")
                     exit()
+                
+                if not index_from in struct_id_energy_dict:
+                    struct_id_energy_dict[index_from] = energy_from
+                if not index_to in struct_id_energy_dict:
+                    struct_id_energy_dict[index_to] = energy_to
 
                 if not index_from in paths_dictionary:
                     paths_dictionary[index_from] = {}
@@ -58,7 +65,7 @@ def read_saddle_csv_file(file):
                 if saddle_energy_kcal < min_saddle:
                     min_saddle = saddle_energy_kcal
                 
-    return paths_dictionary, min_saddle
+    return paths_dictionary, min_saddle, struct_id_energy_dict
 
 if __name__ == "__main__":
     
@@ -68,8 +75,9 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--index_from", action="store", type=int, required=False, help="Index from.")
     parser.add_argument("-j", "--index_to", action="store", type=int, required=False, help="Index to.")
     parser.add_argument("-d", "--structure_index_file", type=str, required=False, help="File with two structures, for which the min max saddle is computed.")
+    parser.add_argument("-p", "--plot", type=str, required=False, help="Create Path Plot.")
     args = parser.parse_args()
-    paths_graph, min_saddle = read_saddle_csv_file(args.file)
+    paths_graph, min_saddle, struct_id_energy_dict = read_saddle_csv_file(args.file)
     
     print(paths_graph)
     
@@ -103,10 +111,49 @@ if __name__ == "__main__":
     
     print(ifrom, ito)
     sp, distances = shortestPath(paths_graph, ifrom, ito)
-
+    #sp.reverse()
+    #distances.reverse()
     print(sp)
     print(min_saddle)
-    print([ distances[x] + min_saddle - 1 for x in sp])
+    #print([ distances[x] + min_saddle - 1 for x in sp])
+    #sp.reverse()
+    path_from_saddle_to = []
+    for i in range(len(sp)):
+        id_from = sp[i]
+        e_from = struct_id_energy_dict[id_from]
+        path_from_saddle_to.append(float(e_from))
+        #print(id_from, e_from)
+        if (i+1 < len(sp)):
+            id_to = sp[i+1]
+            e_saddle = paths_graph[id_from][id_to] + min_saddle - 1
+            path_from_saddle_to.append(float(e_saddle))
+            #print("s", e_saddle)
+            #print(id_to, struct_id_energy_dict[id_to])
+    print(path_from_saddle_to)
+    #print([ (struct_id_energy_dict[x], distances[x] + min_saddle - 1) for x in sp])
+    
+    path_indices = [ x for x in range(1, len(path_from_saddle_to)+1)]
+    
+    all_lines = []
+    lines, = plt.plot(path_indices, path_from_saddle_to, label="Structure Energies", linewidth=1, color = "black", marker=".")
+    all_lines.append(lines)
+    plt.xticks(path_indices)
+    #leg = plt.legend(handles=all_lines, fontsize = 'medium')
+    #leg_texts = leg.get_texts()
+
+    #plt.setp(lines, color='r', linewidth=2.0)
+    #ax = plt.axes()
+    #ax.set_xscale("log", nonposx='clip')
+    #ax.set_yscale("log")
+    plt.xlabel('Structures along Optimal Path')
+    plt.ylabel('Energy [kcal/mol]') 
+    plt.grid(True)
+    res_file="./refolding_path.pdf"
+    plt.savefig(res_file)
+    plt.clf()
+        
+    
+    
 
 
 
